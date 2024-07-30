201971d94fb9e08f90330f7ba4f79f81b1d5cbd4
Kasim Pasaoglu <kasim.pasaoglu@gmail.com>
2024-07-30 04:08:50 +0300

    Hash

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -1,24 +1,27 @@
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import PropTypes from 'prop-types';
 import '../assets/style/Loading.css';
 
 export default function Loading({ onLoadingComplete }) {
     const messageRef = useRef(null);
+    const [showMessage, setShowMessage] = useState(false);
     const [fadeOut, setFadeOut] = useState(false);
 
     useEffect(() => {
         const fadeInTimer = setTimeout(() => {
+            setShowMessage(true);
             if (messageRef.current) {
                 messageRef.current.classList.add('fade-in');
             }
-        }, 500); 
+        }, 1000); // 1 saniye sonra mesajı fade-in yap
 
         const fadeOutTimer = setTimeout(() => {
             setFadeOut(true);
-        }, 4000);
+        }, 4000); // 3 saniye mesaj göründükten sonra loading ekranını fade-out yap
 
         const completeTimer = setTimeout(() => {
             onLoadingComplete();
-        }, 4500);
+        }, 5000); // 4 saniye sonra loading'i tamamla
 
         return () => {
             clearTimeout(fadeInTimer);
@@ -29,7 +32,12 @@ export default function Loading({ onLoadingComplete }) {
 
     return (
         <div className={`loading-screen ${fadeOut ? 'fade-out' : ''}`}>
-            <p ref={messageRef} className="message">&lt;h1&gt;Hello World!&lt;/h1&gt;</p>
+            {showMessage && <h1 ref={messageRef} className="message">&lt;h1&gt;Hello World!&lt;/h1&gt;</h1>}
         </div>
     );
 }
+
+// eslint-disable-next-line react/prop-types
+Loading.propTypes = {
+    onLoadingComplete: PropTypes.func.isRequired,
+};
